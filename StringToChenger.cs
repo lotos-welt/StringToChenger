2f961db22e711b7c221f87e594af91ae9ddb0ba9
Lotos <lotos@lotoslabo.com>
2016-01-20 01:40:52 +0900

    ver1

---
+++ StringToChenger.cs
@@ -0,0 +1,77 @@
+﻿/*!
+ * 文字列変換クラス.
+ * 
+ * @file	StringToChenger.cs
+ * @author	Lotos
+ * @date	2015-11-01 17:23
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+public class StringToChenger : MonoBehaviour {
+
+    /// <summary>
+    /// 変換タイプ.
+    /// </summary>
+    public enum ChangeTypes {
+        None = -1,
+        Upper,
+        Lower
+    }
+
+    /// <summary>
+    /// 変換タイプデフォルト.
+    /// </summary>
+    public ChangeTypes Type = ChangeTypes.None;
+
+    /// <summary>
+    /// 先頭文字を大文字、小文字に変換.
+    /// </summary>
+    /// <param name="text">文字列.</param>
+    /// <param name="type">変換タイプ.</param>
+    /// <returns>変換された文字列.</returns>
+    public static string HeadCharToChanger(string text, int type) {
+        // アルファベットか判断(半角英字か、全角英字か).
+        if (!Regex.IsMatch(text, @"^[ａ-ｚＡ-Ｚ]+$") && !Regex.IsMatch(text, @"^[a-zA-Z]+$")) {
+            return null;
+        }
+        string changedText = string.Empty;
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        switch ((ChangeTypes)(type)) {
+            case ChangeTypes.Upper:
+                changedText = textInfo.ToUpper(text[0]) + text.Substring(1);
+                break;
+            case ChangeTypes.Lower:
+                changedText = textInfo.ToLower(text[0]) + text.Substring(1);
+                break;
+        }
+        return changedText;
+    }
+
+    /// <summary>
+    /// 文字列を大文字、小文字に変換.
+    /// </summary>
+    /// <param name="text">文字列.</param>
+    /// <param name="type">変換タイプ.</param>
+    /// <returns>変換された文字列.</returns>
+    public static string StringToChanger(string text, int type) {
+        // アルファベットか判断(半角英字か、全角英字か).
+        if (!Regex.IsMatch(text, @"^[ａ-ｚＡ-Ｚ]+$") && !Regex.IsMatch(text, @"^[a-zA-Z]+$")) {
+            return null;
+        }
+        string changedText = string.Empty;
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        switch ((ChangeTypes)(type)) {
+            case ChangeTypes.Upper:
+                changedText = textInfo.ToUpper(text);
+                break;
+            case ChangeTypes.Lower:
+                changedText = textInfo.ToLower(text);
+                break;
+        }
+        return changedText;
+    }
+}
